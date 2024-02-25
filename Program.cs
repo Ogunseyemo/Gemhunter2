56fd36caa3b729b6ffd8dcc9c778d6f3142bd356
Ogunseyemo <ogunseyemt@gmail.com>
2024-02-26 00:41:38 +0100

    Create class "player"

--- Program.cs
+++ Program.cs
@@ -12,3 +12,38 @@ class Position
     }
 }
 
+class Player
+{
+    public string Name { get; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+    public int Turns { get; set; }
+
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+        Turns = 0;
+    }
+
+    public void Move(char direction)
+    {
+        Turns++;
+        switch (direction)
+        {
+            case 'U':
+                if (Position.Y > 0) Position.Y--;
+                break;
+            case 'D':
+                if (Position.Y < 5) Position.Y++;
+                break;
+            case 'L':
+                if (Position.X > 0) Position.X--;
+                break;
+            case 'R':
+                if (Position.X < 5) Position.X++;
+                break;
+        }
+    }
+}
