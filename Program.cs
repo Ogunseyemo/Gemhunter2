7f05cb43c81f2860c0417c6bf178253dff135318
Ogunseyemo <ogunseyemt@gmail.com>
2024-02-26 01:01:40 +0100

    create class Board

--- Program.cs
+++ Program.cs
@@ -56,3 +56,107 @@ class Cell
         Occupant = occupant;
     }
 }
+class Board
+{
+    public Cell[,] Grid { get; }
+
+    public Board()
+    {
+        Grid = new Cell[6, 6];
+        InitializeBoard();
+    }
+
+    private void InitializeBoard()
+    {
+        // Initialize cells with empty spaces
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Grid[i, j] = new Cell("-");
+            }
+        }
+
+        // Place players
+        Grid[0, 0].Occupant = "P1";
+        Grid[5, 5].Occupant = "P2";
+
+        // Place gems (random positions)
+        Random random = new Random();
+        for (int i = 0; i < 5; i++)
+        {
+            int gemX = random.Next(6);
+            int gemY = random.Next(6);
+            Grid[gemX, gemY].Occupant = "G";
+        }
+
+        // Place obstacles (random positions)
+        for (int i = 0; i < 8; i++)
+        {
+            int obstacleX = random.Next(6);
+            int obstacleY = random.Next(6);
+            // Make sure not to overwrite players or gems
+            if (Grid[obstacleX, obstacleY].Occupant == "-")
+            {
+                Grid[obstacleX, obstacleY].Occupant = "O";
+            }
+            else
+            {
+                i--;
+            }
+        }
+    }
+
+    public void Display()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Console.Write($"{Grid[i, j].Occupant} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public bool IsValidMove(Player player, char direction)
+    {
+        int newX = player.Position.X;
+        int newY = player.Position.Y;
+
+        switch (direction)
+        {
+            case 'U':
+                newY--;
+                break;
+            case 'D':
+                newY++;
+                break;
+            case 'L':
+                newX--;
+                break;
+            case 'R':
+                newX++;
+                break;
+        }
+
+        // Check if the new position is within bounds and not an obstacle
+        return newX >= 0 && newX < 6 && newY >= 0 && newY < 6 && Grid[newY, newX].Occupant != "O";
+    }
+
+    public void UpdatePlayerPosition(Player player, Position previousPosition)
+    {
+        Grid[previousPosition.Y, previousPosition.X].Occupant = "-";
+        Grid[player.Position.Y, player.Position.X].Occupant = player.Name;
+    }
+
+    public void CollectGem(Player player)
+    {
+        if (Grid[player.Position.Y, player.Position.X].Occupant == "G")
+        {
+            player.GemCount++;
+            Grid[player.Position.Y, player.Position.X].Occupant = "-";
+            Console.WriteLine($"{player.Name} found a gem!");
+        }
+    }
+}
